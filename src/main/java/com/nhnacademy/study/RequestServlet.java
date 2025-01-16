9edbb62604010e793e02716d913e13aaf1a7c390
ATGN02-018 <ygw1026@naver.com>
2025-01-16 17:08:19 +0900

    Day1-10 실습1 완료

--- src/main/java/com/nhnacademy/study/RequestServlet.java
+++ src/main/java/com/nhnacademy/study/RequestServlet.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.study;
 
+import ch.qos.logback.core.net.AutoFlushingObjectWriter;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -7,25 +8,49 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Objects;
 
 @Slf4j
 public class RequestServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        resp.resetBuffer();
+
+        log.info("default buffer size : {}", resp.getBufferSize());
+        resp.setBufferSize(1024);
+
         resp.setContentType("text/plain");
         resp.setCharacterEncoding("UTF-8");
 
         try(PrintWriter out = resp.getWriter()){
             out.println("locale=" + req.getLocale());
             out.println("parameter name=" + req.getParameter("userId"));
-            out.println("content type=" + req.getContentType());
-            out.println("content length=" + req.getContentLengthLong());
+//            out.flush();
+//            out.close();
+
+            String userId = req.getParameter("userId");
+            log.info("userId:{}", userId);
+            if(userId == null || userId.isEmpty()){
+                resp.reset();
+                resp.setStatus(500);
+                resp.sendError(500, "name is empty");
+                return;
+            }
+
+            String redirect = req.getParameter("redirect");
+            if(Objects.nonNull(redirect)){
+                resp.sendRedirect(redirect);
+                return;
+            }
+
             out.println("method=" + req.getMethod());
-            out.println("servlet path=" + req.getServletPath());
             out.println("request uri=" + req.getRequestURI());
-            out.println("request uri=" + req.getRequestURL());
+
+
             out.println("User-Agent header=" + req.getHeader("User-Agent"));
+            out.flush();
         }catch (Exception e){
             log.error("/req : {}", e.getMessage(),e);
         }
