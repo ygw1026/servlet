a9a6327c1c834b8b3a753878a662209ac503e191
ATGN02-018 <ygw1026@naver.com>
2025-01-16 14:54:33 +0900

    Day1-8 실습2 완료

--- src/main/java/com/nhnacademy/study/CounterUtils.java
+++ src/main/java/com/nhnacademy/study/CounterUtils.java
@@ -1,5 +1,16 @@
 package com.nhnacademy.study;
 
-public final class CounterUtils {
+import jakarta.servlet.ServletContext;
 
+import java.util.Optional;
+
+public final class CounterUtils {
+    private CounterUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+    public static void increaseCounter(ServletContext servletContext){
+        Long counter = Optional.ofNullable((Long)servletContext.getAttribute("counter")).orElse(0L);
+        counter = counter + 1;
+        servletContext.setAttribute("counter",counter);
+    }
 }
