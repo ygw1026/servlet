3f9c9701e5bb2418dfb035940fff77160e2022af
ATGN02-018 <ygw1026@naver.com>
2025-01-16 14:14:46 +0900

    Day1-8 실습1 완료

---
+++ src/main/java/com/nhnacademy/study/CounterUtils.java
@@ -0,0 +1,5 @@
+package com.nhnacademy.study;
+
+public final class CounterUtils {
+
+}
--- src/main/java/com/nhnacademy/study/MultipleServlet.java
+++ src/main/java/com/nhnacademy/study/MultipleServlet.java
@@ -1,5 +1,7 @@
 package com.nhnacademy.study;
 
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,9 +16,10 @@ public class MultipleServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String[] values = req.getParameterValues("class");
-
+        String url = getServletContext().getInitParameter("url");
         try(PrintWriter out = resp.getWriter()){
             out.println(String.join(",", values));
+            out.printf("url:%s\n", url);
         }catch (IOException e) {
             throw new RuntimeException(e);
         }
